a3f3b8401f8a77af9d7a5a37e84e829febb240ba
zhongjef <jefferson.zhong@mail.utoronto.ca>
2020-11-30 21:34:46 -0800

    Implement connect and disconnect - Malay, Jeff

--- place/src/Canvas.js
+++ place/src/Canvas.js
@@ -1,6 +1,24 @@
 import React, { Component } from "react";
+import { getWSService } from './websocket';
+
 
 class Canvas extends Component {
+    state = {
+        ws: null
+    }
+
+    componentDidMount() {
+        // connect to websocket
+        this.setState({
+            ws: getWSService()
+        })
+        // api call to get board
+
+    }
+    componentWillUnmount() {
+        this.state.ws.onclose();
+    }
+
     constructor(props){
         super(props);
         this.setPixel = this.setPixel.bind(this);
@@ -10,16 +28,20 @@ class Canvas extends Component {
         var x = document.getElementById("x").value;
         var y = document.getElementById("y").value;
         var color = document.getElementById("color").value;
-        var canvas = document.getElementById("myCanvas");
-        var ctx = canvas.getContext("2d");
-        ctx.fillStyle = color;
-        ctx.fillRect(x, y, 1, 1 );
+        var username = document.getElementById("username").value;
+        // var canvas = document.getElementById("myCanvas");
+        // var ctx = canvas.getContext("2d");
+        // ctx.fillStyle = color;
+        // ctx.fillRect(x, y, 1, 1 );
     }
 
     render() {
         return (
             <div>
                 <canvas id="myCanvas" width="250" height="250"></canvas>
+                <br/>
+                <label>Username: </label>
+                <input type="text" id="username" name="username" />
                 <br/>
                 <label>X Coordinate: </label>
                 <input type="number" id="x" name="x" min="0" max="250"/>
@@ -36,6 +58,8 @@ class Canvas extends Component {
             </div>
         );
     }
+
 }
+
 
 export default Canvas;
