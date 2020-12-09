240898a1bde89611d9bfd14e1d609c6c142538a6
zhongjef <jefferson.zhong@mail.utoronto.ca>
2020-12-08 21:54:38 -0800

    Optimize gnuParallel algorithm

--- search/gnuParallel/parallel.bash
+++ search/gnuParallel/parallel.bash
@@ -1,32 +1,29 @@
-# TARGET_STRING
-ALPHABET="hello"
-TARGET="hello"
-len=${#ALPHABET}
+TARGET=$1
+ALPHABET=$2
+RESULT_FILE="spaced_combos"
+
+TARGET_LEN=${#TARGET}
+
+indexing=""
+for ((n=1; n < ($TARGET_LEN + 1); n++));
+do
+    indexing="${indexing}{$n}"
+done
+
 
 param_list=""
-for ((n=0; n < ($len); n++));
+for ((m=0; m < ($TARGET_LEN); m++));
 do
     param_list="${param_list} ::: $(echo $ALPHABET | sed 's/.\{1\}/& /g')"
 done
 
-parallel "echo {}" $param_list > spaced_combos
+parallel "echo $indexing" $param_list > $RESULT_FILE
 
-
-indexing=""
-for ((m=1; m < ($len + 1); m++));
-do
-    indexing="${indexing}{$m}"
-done
-
-# gcc -o search ./search.c
-# parallel --colsep ' ' "./search $indexing $TARGET" :::: spaced_combos > results
-
-parallel --colsep ' ' "echo $indexing" :::: spaced_combos > unspaced_combos
-found=$(cat unspaced_combos | grep "$TARGET")
+found="$(parallel grep $TARGET ::: $RESULT_FILE | head -n 1)"
 if [ -z "$found" ]; then
     echo "not found"
 else
     echo "found"
 fi
 
-rm -f ./*spaced_combos
+# rm -f $RESULT_FILE
