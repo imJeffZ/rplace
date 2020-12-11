c8071d702541610185416cd61aa628696448337d
Jefferson Zhong <zhongjef@dh2020pc24.utm.utoronto.ca>
2020-12-10 20:24:17 -0500

    Starting a new implementation of gnuparallel algorithm

--- search/gnuParallel/parallel.bash
+++ search/gnuParallel/parallel.bash
@@ -1,29 +1,30 @@
-TARGET=$1
-ALPHABET=$2
-RESULT_FILE="spaced_combos"
+parallel < jobs
+# TARGET=$1
+# ALPHABET=$2
+# RESULT_FILE="spaced_combos"
 
-TARGET_LEN=${#TARGET}
+# TARGET_LEN=${#TARGET}
 
-indexing=""
-for ((n=1; n < ($TARGET_LEN + 1); n++));
-do
-    indexing="${indexing}{$n}"
-done
+# indexing=""
+# for ((n=1; n < ($TARGET_LEN + 1); n++));
+# do
+#     indexing="${indexing}{$n}"
+# done
 
 
-param_list=""
-for ((m=0; m < ($TARGET_LEN); m++));
-do
-    param_list="${param_list} ::: $(echo $ALPHABET | sed 's/.\{1\}/& /g')"
-done
+# param_list=""
+# for ((m=0; m < ($TARGET_LEN); m++));
+# do
+#     param_list="${param_list} ::: $(echo $ALPHABET | sed 's/.\{1\}/& /g')"
+# done
 
-parallel "echo $indexing" $param_list > $RESULT_FILE
+# parallel "echo $indexing" $param_list > $RESULT_FILE
 
-found="$(parallel grep $TARGET ::: $RESULT_FILE | head -n 1)"
-if [ -z "$found" ]; then
-    echo "not found"
-else
-    echo "found"
-fi
+# found="$(parallel grep $TARGET ::: $RESULT_FILE | head -n 1)"
+# if [ -z "$found" ]; then
+#     echo "not found"
+# else
+#     echo "found"
+# fi
 
 # rm -f $RESULT_FILE
